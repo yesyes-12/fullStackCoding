f84a88c8f65f791a0dc938a481d55a7b785a031d
여인범 <66177781+yesyes-12@users.noreply.github.com>
2024-04-17 02:06:01 +0900

    Delete python_web_scaper directory

--- python_web_scaper/build_website_with_flask/file.py
+++
@@ -1,9 +0,0 @@
-def save_to_file(filename, jobs):
-    file = open(f"{filename}.csv", mode="w", encoding="utf-8", newline = "")
-    file.write("Title, Company, Location, Link\n")
-    
-    for job in jobs:
-        file.write(f"{job['title']}, {job['company']}, \
-            {job['location']}, {job['link']}\n")
-        
-    file.close()
--- python_web_scaper/build_website_with_flask/main.py
+++
@@ -1,9 +0,0 @@
-#from extractors.indeed import extract_indeed_jobs
-from extractors.remoteok import extract_remoteok_jobs
-from file import save_to_file
-
-keyword = input("What do you want to search for? ")
-
-remoteok = extract_remoteok_jobs(keyword)
-
-save_to_file(keyword, remoteok)
